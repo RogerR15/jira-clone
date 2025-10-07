6432995ba7cc592ffa9789a23d1fb21d831320a1
unknown <robertpopa015@gmail.com>
2025-10-07 20:03:01 +0300

    tasks API

--- src/app/(dashboard)/workspaces/[workspaceId]/projects/[projectId]/page.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/projects/[projectId]/page.tsx
@@ -2,6 +2,7 @@ import { Button } from "@/components/ui/button";
 import { getCurrent } from "@/features/auth/queries";
 import { ProjectAvatar } from "@/features/projects/components/project-avatar";
 import { getProject } from "@/features/projects/queries";
+import { TaskViewSwitcher } from "@/features/tasks/components/task-view-switcher";
 import { PencilIcon } from "lucide-react";
 import Link from "next/link";
 import { redirect } from "next/navigation";
@@ -50,7 +51,7 @@ const ProjectsPage = async ({ params }: ProjectsPageProps) => {
                     </Button>
                 </div>
             </div>
-
+            <TaskViewSwitcher />
         </div>
     );
 }
